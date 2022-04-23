6ad21bcd511d24957750d14da64df36dc2c584a5
evyatarweiss <67328042+evyatarweiss@users.noreply.github.com>
2022-04-23 12:42:35 +0300

    Add files via upload

---
+++ ParkingLotAssignment/ParkingLotObjects/EntryObject.cs
@@ -0,0 +1,8 @@
+﻿namespace ParkingLotAssignment.ParkingLotObjects
+{
+    public class EntryObject
+    {
+        public string licensePlate { get; set; }
+        public int parkingLotID { get; set; }
+    }
+}
---
+++ ParkingLotAssignment/ParkingLotObjects/ExitObject.cs
@@ -0,0 +1,11 @@
+﻿namespace ParkingLotAssignment.ParkingLotObjects
+{
+    public class ExitObject
+    {
+        public string licensePlate { get; set; }
+        public int parkingLotID { get; set; }
+        public int TotalPrice { get; set; }
+        public int TotalParkTime { get; set; }
+
+    }
+}
